94961417f77a2089fd0f240822ac12ea2df51bc6
zengliq <zengliq@homeking365.com>
2025-09-12 17:29:26 +0800

    feat: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { House, CirclePlusFilled  } from '@element-plus/icons-vue'
+
+</script>
+
+<template>
+  <el-icon><CirclePlusFilled /></el-icon>
+  <el-icon><House /></el-icon>
+   <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
